5ed16148de9a3d60ad8202b332842aaa8034659c
FIRST_NAME LAST_NAME <intezar@vicdigit-technologies.com>
2023-01-24 17:26:04 +0530

    zunaid HBD

---
+++ src/data/data.js
@@ -0,0 +1,46 @@
+export const data = {
+  person: {
+    name: "Zunaid Beta",
+    dob: "26 Jan 2020",
+    age: "3",
+    image: "5.jpeg",
+  },
+  leftSideCards: [
+    {
+      image: "1.jpeg",
+      message:
+        "कामियाबी के हर शिखर पर आपका नाम हो,        आपके हर कदम पर सफलता का साथ हो,        बेटे मुश्किलों का हिम्मत से सामना करना,        एक दिन पूरी दुनिया आपके नाम हो।        जन्मदिन की बहुत-बहुत बधाईयाँ!",
+      textColor: "#196F3D ",
+    },
+    {
+      image: "2.jpeg",
+      message:
+        "खुशियों से भरी हो जिन्दगी मुस्कुराहट से भरा हो हर पल यह जिन्दगी कम न पड़ जाए इतनी खुशियाँ दे आपको आने वाला कल! मेरी दुआ है कि तुम हमेशा ऐसे ही चमकते रहो और दुनिया में अपनी रौशनी फैलते रहो Happy Birthday Zunaid",
+    },
+    {
+      image: "7.jpeg",
+      message:
+        "ऐसी क्या दुआ दूँ आपको, जो आपके लबों पर ख़ुशी के फूल खिला दे,बस ये दुआ है मेरी,सितारों सी रौशनी खुदा आपकी तकदीर बना दे।  हैप्पी बर्थ डे Zunaid",
+    },
+  ],
+  rightSideCards: [
+    {
+      image: "4.jpeg",
+      message:
+        "Happy birthday to the future superstar! One day you will become a global icon.!On this day a little prince was born. And that handsome prince is you. Happy Birthday Zunaid!",
+    },
+    {
+      image: "5.jpeg",
+      message:
+        " खुशियों से भरी हो जीवन आपका,  आपका नन्नी मुस्कान पर हर कोई फिदा रहे दुआ करता हूं मैं रब से कि तुम्हारे जीवन से हर मुश्किल जुदा रहे Wishing You Happy Birthday Little Boy"
+    },
+    {
+      image: "6.jpeg",
+      message:
+        "Be happy! Today is the day you were brought into this world to be a blessing and inspiration to the people around you! You are a wonderful person! May you be given more birthdays to fulfill all of your dreams!",
+    },
+  ],
+  author: {
+    name: "Md Intezar Alam",
+  },
+};
